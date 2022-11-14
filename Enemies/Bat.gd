caaa9d3b1a691162257347bf0f040ddff2ad069c
Gabriel Mack <69842642+GabeeeM@users.noreply.github.com>
2022-11-14 11:54:16 -0800

    updated quite a bitsy!
    the health is no t working at all

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,21 +1,62 @@
 extends KinematicBody2D
 
 var knockback = Vector2.ZERO
+var velocity = Vector2.ZERO
+
+onready var sprite = $AnimatedSprite
+onready var playerDetectionZone = $PlayerDetectionZone
 onready var stats = $Stats
+onready var hurtbox = $Hurtbox
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 75
+export var FRICTION = 100
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var state = IDLE
 
 func _ready():
 	print(stats.max_health)
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+		
+		WANDER:
+			pass
+		
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				sprite.flip_h = velocity.x < 0
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+		
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	print(stats.health)
 	knockback = area.knockback_vector * 120
+	hurtbox.create_hit_effect()
 
 
 func _on_Stats_no_health():
